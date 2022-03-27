0e57a6636622faf7418262592274caf6e483f08f
ivillamizar5 <ivan.151867@gmail.com>
2022-03-27 16:53:50 -0500

    version 1

---
+++ Piano/script.js
@@ -0,0 +1,43 @@
+
+
+function btnDo(){
+    var sound = new Audio();
+    sound.src = "./musica/DO.wav";
+    sound.play();
+}
+
+function btnRe(){
+    var sound = new Audio();
+    sound.src = "./musica/RE.wav";
+    sound.play();
+}
+
+function btnMi(){
+    var sound = new Audio();
+    sound.src = "./musica/MI.wav";
+    sound.play();
+}
+
+function btnFa(){
+    var sound = new Audio();
+    sound.src = "./musica/FA.wav";
+    sound.play();
+}
+
+function btnSol(){
+    var sound = new Audio();
+    sound.src = "./musica/Sol.wav";
+    sound.play();
+}
+
+function btnLa(){
+    var sound = new Audio();
+    sound.src = "./musica/LA.wav";
+    sound.play();
+}
+
+function btnSi(){
+    var sound = new Audio();
+    sound.src = "./musica/SI.wav";
+    sound.play();
+}
